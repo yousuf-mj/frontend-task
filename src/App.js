71d8a48ee6c8d51551b737f804ea8d9baa095d12
yousuf mj <yousuf.jaleel@dvcorporate.com>
2020-03-27 09:13:50 +0000

    render components together

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import Card from './containers/Card/Card'
 function App() {
 	return (
 		<div className="app">
-			<Card/>
+			<Card
+                id={1}
+                title="React Developer"
+                salary="75000"
+                type="Permanent"
+                location="London"
+                description="React Developer Circa £75,000 City of London - Marylebone (NW1) The React Developer will work on a re-platforming of existing products ..."
+                logo="https://logo.clearbit.com/company.com"
+                company="Acme Company"
+            />
 		</div>
 	);
 }
--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -2,11 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Card.scss';
+import {Title} from '../../components/Title'
+import {Details} from '../../components/Details'
+import {Button} from '../../components/Button'
+import {CompanyLogo} from '../../components/CompanyLogo'
+import {Description} from '../../components/Description'
 
 const Card = props => {
 	return (
 		<div className="job-card">
-            Test 
+            <Title title={props.title}/>
+            <div className="job-details">
+                <Details 
+                    salary={props.salary} 
+                    type={props.type}
+                    location={props.location}
+                    company={props.company}
+                    />
+                <CompanyLogo 
+                    logo={props.logo} 
+                    company={props.company}
+                />
+            </div>
+            <Description description={props.description}/>
+            <Button/>
 		</div>
 	);
 };
